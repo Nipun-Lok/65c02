fdd8e5b30db6436e362077af60c16f3d871ca929
Nipun Lokuwithanage <nipun.lok@gmail.com>
2026-02-22 13:23:39 +1100

    build.sh supports multiple .s files

--- src/build.sh
+++ src/build.sh
@@ -1,8 +1,52 @@
 #!/bin/bash
 
+if [ $# -eq 0 ]; then
+    echo "Error: Missing compile method"
+    echo "Usage: ./build.sh <vasm|cc65> source.s [config.cfg]"
+    exit 1
+fi
+
 COMPILER=$1
-SRC=$2
-CFG=$3
+SRC=""
+CFG=""
+
+if [ "$COMPILER" != "vasm" ] && [ "$COMPILER" != "cc65" ]; then
+    echo "Error: Missing compile method"
+    echo "Usage: ./build.sh <vasm|cc65> source.s [config.cfg]"
+    exit 1
+fi
+
+shift
+
+for arg in "$@"; do
+    case "$arg" in
+        *.s)
+            if [ -z "$SRC" ]; then
+                SRC="$arg"
+            elif [ "$COMPILER" = "vasm" ]; then
+                echo "Error: Too many arguments"
+                echo "Usage: ./build vasm source.s"
+                exit 1
+            else
+                SRC="$SRC $arg"
+            fi
+            ;;
+        *.cfg)
+            if [ "$COMPILER" = "vasm" ]; then
+                echo "Error: Invalid input"
+                echo "Usage: ./build vasm source.s"
+                exit 1
+            else
+                CFG="$arg"
+            fi
+            ;;
+        *)
+            echo "Error: Unrecognised file"
+            exit 1
+            ;;
+    esac
+done
+
 
 if [ -z "$COMPILER" ]; then
     echo "Error: Missing compile method"
